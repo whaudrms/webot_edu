5978231cf3a50d6b2d6e0d548acdd40b2318b237
whaudrms <myeongeunjo9@gmail.com>
2025-07-30 17:09:44 +0900

    2nd class

--- src/test/scripts/08_sub_class_lidar.py
+++ src/test/scripts/08_sub_class_lidar.py
@@ -1,5 +1,6 @@
 import rospy
 from sensor_msgs.msg import LaserScan
+from math import *
 
 class Sub_class:
     def __init__(self):
@@ -7,8 +8,17 @@ class Sub_class:
         rospy.Subscriber("/lidar2D",LaserScan,callback=self.callback)
 
     def callback(self,msg:LaserScan): 
+        degree_min = degrees(msg.angle_min)
+        degree_max = degrees(msg.angle_max)
+        degree_increment = degrees(msg.angle_increment)
+
+        cal_degrees = [degree_min+degree_increment*index for index,value in enumerate(msg.ranges)]
+
         print("-------------------")
-        print(msg)
+        print(degree_min)
+        print(degree_max)
+        print(degree_increment)
+        print(cal_degrees)
 
 if __name__ == "__main__":
     pub_node = Sub_class()
